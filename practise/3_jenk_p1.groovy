a0da246df35257fc18c9b841dd2dac504632c0d5
27kittu <cv27.one@gmail.com>
2025-02-03 22:01:46 +0530

    added jenkins docker script modified

--- practise/3_jenk_p1.groovy
+++ practise/3_jenk_p1.groovy
@@ -2,11 +2,35 @@ pipeline {
   agent any
   tools {
     git 'Default'
+    maven 'maven_399'
   }
   stages {
     stage('gitScm') {
       steps {
-        git credentailsId: 'trainyou_cred', url: 'https://github.com/trainyou/spring-boot-mongo-docker-pvt.git' 
+        git credentialsId: 'trainyou_cred', url: 'https://github.com/trainyou/spring-boot-mongo-docker-pvt.git'
+      }
+    }
+
+    stage('buildPkg') {
+      steps {
+        sh "mvn clean validate \
+        && mvn clean compile \
+        && mvn clean test \
+        && mvn clean package"
+      }
+    }
+
+    stage('buildDockImage') {
+      steps {
+        sh "docker build -t heartocean/cat:$BUILD_NUMBER ."
+      }
+    }
+
+    stage('imageUploadReg') {
+      steps {
+        withCredentials([usernamePassword(credentialsId: 'docker_cred', passwordVariable: 'docker_pass', usernameVariable: 'docker_username')]) {
+          sh "docker push heartocean/cat:$BUILD_NUMBER"
+        }
       }
     }
   }
